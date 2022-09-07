af07167f42c281e0c43da0b939119bac90b45bef
harsh <harshitpandeyfeelings@gmail.com>
2022-09-07 11:39:05 +0530

    Added a new java file

---
+++ Java/lab_projects/exp_4/Exp_4.java
@@ -0,0 +1,39 @@
+class Q1_parent
+{
+    private int a=10;
+    private void change(int a)
+    {
+        this.a=a;
+    }
+    void disp()
+    {
+        System.out.println(a);
+    } 
+}
+class Q1_child extends Q1_parent
+{
+    int b=10;
+    Q1_child()
+    {
+        disp();
+    }
+    void enter(int a)
+    {
+        change(a);
+    }
+    void enter_direct(int x,int y)
+    {
+        b=y;
+        a=x;
+    }
+    public static void main(String[]args)
+    {
+        Q1_child obj=new Q1_child();
+        obj.enter(10);
+        obj.enter2(20,30);
+    }
+}
+class Exp_4
+{
+    
+}
